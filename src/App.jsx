826763005fd6f27970ba00a51eaf319576254339
Artem <feed03@yandex.ru>
2024-05-16 00:31:53 +0300

    Add routes for orders

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import ListPlaygroundsComponent from "./components/ListPlaygroundsComponent.jsx"
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import PlaygroundComponent from "./components/PlaygroundComponent.jsx";
-import RegistrationComponent from "./components/RegistrationComponent.jsx";
 import LoginComponent from "./components/LoginComponent.jsx";
 import AuthOutlet from '@auth-kit/react-router/AuthOutlet';
+import OrderComponent from "./components/OrderComponent.jsx";
+import ListOrdersComponent from "./components/ListOrdersComponent.jsx";
+import RegistrationComponent from "./components/RegistrationComponent.jsx";
 
 function App() {
 
@@ -20,6 +22,9 @@ function App() {
                     <Route path='/playgrounds' element={< ListPlaygroundsComponent /> }/>
                     <Route path='/add-playground' element={< PlaygroundComponent/>}/>
                     <Route path='/edit-playground/:id'  element={< PlaygroundComponent/>}/>
+                    <Route path='/playgrounds/:id'  element={< OrderComponent/>}/>
+                    <Route path='/orders' element={< ListOrdersComponent/>}/>
+                    <Route path='/edit-order/:order_id'  element={< OrderComponent/>}/>
                  </Route>
                  <Route path='/register' element={< RegistrationComponent/>}/>
                  <Route path='/login' element={< LoginComponent/>}/>
